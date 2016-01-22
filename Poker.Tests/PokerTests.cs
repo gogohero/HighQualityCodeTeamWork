bd40981623640d58138bcd1446ad4947e34129dd
Krasimir Nedev <krasi.nedew@gmail.com>
2016-01-23 00:11:23 +0200

    New card logic calculator, unit tests for it (works fine - recognizes all 10 winning types, still to be tested in corner cases) with good design and good code readability. Still not implemented in the spaghetti code

--- Poker.Tests/PokerTests.cs
+++ Poker.Tests/PokerTests.cs
@@ -12,7 +12,7 @@ namespace Poker.Tests
     public class PokerTests
     {
         [TestMethod]
-        public void TestingAlgorithm_Calculate_Card_Power()
+        public void TestingAlgorithm_Calculate_Card_Power_FourOfAKind_ShouldPass()
         {
             ICard aceOfSpades = new Card(12, 'S');
             ICard aceOfClubs = new Card(12, 'C');
@@ -23,6 +23,141 @@ namespace Poker.Tests
             hand.CurrentCards = fourOfAKind;
             CardPowerCalculator.GetCurrentStrengthOfCards(hand);
             Assert.AreEqual(HandStrengthEnum.FourOfAKind, hand.Strength);
+        }
+
+        [TestMethod]
+        public void TestingAlgorithm_Calculate_Card_Power_RoyalFlush_ShouldPass()
+        {
+            ICard aceOfSpades = new Card(12, 'S');
+            ICard kingOfSpades = new Card(11, 'S');
+            ICard queenOfSpades = new Card(10, 'S');
+            ICard jokerOfSpades = new Card(9, 'S');
+            ICard tenOfSpades = new Card(8, 'S');
+            IHand hand = new Hand();
+            IList<ICard> royalFlush = new List<ICard>() { aceOfSpades, kingOfSpades, queenOfSpades, jokerOfSpades, tenOfSpades };
+            hand.CurrentCards = royalFlush;
+            CardPowerCalculator.GetCurrentStrengthOfCards(hand);
+            Assert.AreEqual(HandStrengthEnum.RoyalFlush, hand.Strength);
+        }
+
+        [TestMethod]
+        public void TestingAlgorithm_Calculate_Card_Power_FullHouse_ShouldPass()
+        {
+            ICard card1 = new Card(12, 'S');
+            ICard card2 = new Card(12, 'D');
+            ICard card3 = new Card(12, 'H');
+            ICard card4 = new Card(10, 'S');
+            ICard card5 = new Card(10, 'C');
+            IHand hand = new Hand();
+            IList<ICard> fullHouse = new List<ICard>() { card1, card2, card3, card4, card5 };
+            hand.CurrentCards = fullHouse;
+            CardPowerCalculator.GetCurrentStrengthOfCards(hand);
+            Assert.AreEqual(HandStrengthEnum.FullHouse, hand.Strength);
+        }
+
+        [TestMethod]
+        public void TestingAlgorithm_Calculate_Card_Power_ThreeOfAKind_ShouldPass()
+        {
+            ICard card1 = new Card(12, 'S');
+            ICard card2 = new Card(12, 'D');
+            ICard card3 = new Card(12, 'H');
+            ICard card4 = new Card(10, 'S');
+            ICard card5 = new Card(9, 'C');
+            IHand hand = new Hand();
+            IList<ICard> fullHouse = new List<ICard>() { card1, card2, card3, card4, card5 };
+            hand.CurrentCards = fullHouse;
+            CardPowerCalculator.GetCurrentStrengthOfCards(hand);
+            Assert.AreEqual(HandStrengthEnum.ThreeOfAKind, hand.Strength);
+        }
+
+        [TestMethod]
+        public void TestingAlgorithm_Calculate_Card_Power_TwoPair_ShouldPass()
+        {
+            ICard card1 = new Card(12, 'S');
+            ICard card2 = new Card(9, 'D');
+            ICard card3 = new Card(12, 'H');
+            ICard card4 = new Card(10, 'S');
+            ICard card5 = new Card(9, 'C');
+            IHand hand = new Hand();
+            IList<ICard> fullHouse = new List<ICard>() { card1, card2, card3, card4, card5 };
+            hand.CurrentCards = fullHouse;
+            CardPowerCalculator.GetCurrentStrengthOfCards(hand);
+            Assert.AreEqual(HandStrengthEnum.TwoPair, hand.Strength);
+        }
+
+        [TestMethod]
+        public void TestingAlgorithm_Calculate_Card_Power_Pair_ShouldPass()
+        {
+            ICard card1 = new Card(12, 'S');
+            ICard card2 = new Card(9, 'D');
+            ICard card3 = new Card(5, 'H');
+            ICard card4 = new Card(10, 'S');
+            ICard card5 = new Card(9, 'C');
+            IHand hand = new Hand();
+            IList<ICard> fullHouse = new List<ICard>() { card1, card2, card3, card4, card5 };
+            hand.CurrentCards = fullHouse;
+            CardPowerCalculator.GetCurrentStrengthOfCards(hand);
+            Assert.AreEqual(HandStrengthEnum.Pair, hand.Strength);
+        }
+
+        [TestMethod]
+        public void TestingAlgorithm_Calculate_Card_Power_Flush_ShouldPass()
+        {
+            ICard card1 = new Card(12, 'S');
+            ICard card2 = new Card(9, 'S');
+            ICard card3 = new Card(5, 'S');
+            ICard card4 = new Card(10, 'S');
+            ICard card5 = new Card(9, 'S');
+            IHand hand = new Hand();
+            IList<ICard> fullHouse = new List<ICard>() { card1, card2, card3, card4, card5 };
+            hand.CurrentCards = fullHouse;
+            CardPowerCalculator.GetCurrentStrengthOfCards(hand);
+            Assert.AreEqual(HandStrengthEnum.Flush, hand.Strength);
+        }
+
+        [TestMethod]
+        public void TestingAlgorithm_Calculate_Card_Power_Straigth_ShouldPass()
+        {
+            ICard card1 = new Card(12, 'S');
+            ICard card2 = new Card(11, 'C');
+            ICard card3 = new Card(10, 'H');
+            ICard card4 = new Card(9, 'S');
+            ICard card5 = new Card(8, 'S');
+            IHand hand = new Hand();
+            IList<ICard> fullHouse = new List<ICard>() { card1, card2, card3, card4, card5 };
+            hand.CurrentCards = fullHouse;
+            CardPowerCalculator.GetCurrentStrengthOfCards(hand);
+            Assert.AreEqual(HandStrengthEnum.Straight, hand.Strength);
+        }
+
+        [TestMethod]
+        public void TestingAlgorithm_Calculate_Card_Power_HighCard_ShouldPass()
+        {
+            ICard card1 = new Card(12, 'S');
+            ICard card2 = new Card(11, 'C');
+            ICard card3 = new Card(6, 'H');
+            ICard card4 = new Card(9, 'S');
+            ICard card5 = new Card(8, 'S');
+            IHand hand = new Hand();
+            IList<ICard> fullHouse = new List<ICard>() { card1, card2, card3, card4, card5 };
+            hand.CurrentCards = fullHouse;
+            CardPowerCalculator.GetCurrentStrengthOfCards(hand);
+            Assert.AreEqual(HandStrengthEnum.HighCard, hand.Strength);
+        }
+
+        [TestMethod]
+        public void TestingAlgorithm_Calculate_Card_Power_StraigthFlush_ShouldPass()
+        {
+            ICard card1 = new Card(11, 'S');
+            ICard card2 = new Card(10, 'S');
+            ICard card3 = new Card(9, 'S');
+            ICard card4 = new Card(8, 'S');
+            ICard card5 = new Card(7, 'S');
+            IHand hand = new Hand();
+            IList<ICard> fullHouse = new List<ICard>() { card1, card2, card3, card4, card5 };
+            hand.CurrentCards = fullHouse;
+            CardPowerCalculator.GetCurrentStrengthOfCards(hand);
+            Assert.AreEqual(HandStrengthEnum.StraightFlush, hand.Strength);
         }
     }
 }
--- Poker/TestingAlgorithms/CardPowerCalculator.cs
+++ Poker/TestingAlgorithms/CardPowerCalculator.cs
@@ -14,89 +14,69 @@
         // method for setting the strength of any participant current visible hand (his cards in hand + facing up cards from the board)
         public static void GetCurrentStrengthOfCards(IHand participantVisibleHand)
         {
+            // getting initial 2 cards in hand
             ICard[] startingTwoCards = 
                  {
                     participantVisibleHand.CurrentCards[0],
                     participantVisibleHand.CurrentCards[1]
                  };
 
-            IList<ICard> differentCards = new List<ICard>();
-
-            foreach (var card in participantVisibleHand.CurrentCards)
-            {
-                if (differentCards.Any(c => c.Rank == card.Rank))
-                {
-                    continue;
-                }
-
-                differentCards.Add(card);
-            }
-
-            // Check for pair, two pair, three of a kind, full house or four of a kind
-            for (int i = 0; i < differentCards.Count; i++)
-            {
-                int pairedCardsCounter = participantVisibleHand.CurrentCards.Count(card => card.Rank == differentCards[i].Rank);
-
-                if (pairedCardsCounter > 1)
-                {
-                    // four of a kind check
-                    if (pairedCardsCounter == 4)
-                    {
-                        participantVisibleHand.Strength = HandStrengthEnum.FourOfAKind;
-                        participantVisibleHand.HighCard = differentCards[i];
-                        break;
-                    }
-
-                    // three of a kind check
-                    if (pairedCardsCounter == 3 && (int)participantVisibleHand.Strength < pairedCardsCounter)
-                    {
-                        participantVisibleHand.Strength  = HandStrengthEnum.ThreeOfAKind;
-                        participantVisibleHand.HighCard = differentCards[i];
-                    }
-
-                    // check full house -> if not check two pair -> if not results in simple pair
-                    if (pairedCardsCounter == 2 
-                        && participantVisibleHand.Strength == HandStrengthEnum.ThreeOfAKind 
-                        && (int)participantVisibleHand.Strength < (int)HandStrengthEnum.FourOfAKind)
-                    {
-                        participantVisibleHand.Strength = HandStrengthEnum.FullHouse;
-                    }
-                    else if (participantVisibleHand.Strength == HandStrengthEnum.Pair)
-                    {
-                        participantVisibleHand.Strength = HandStrengthEnum.TwoPair;
-                        if (participantVisibleHand.HighCard.Rank < differentCards[i].Rank)
-                        {
-                            participantVisibleHand.HighCard = differentCards[i];
-                        }
-                    }
-                    else
-                    {
-                        participantVisibleHand.Strength = HandStrengthEnum.Pair;
-                        participantVisibleHand.HighCard = differentCards[i];
-                    }
-                }
-            }
-
+            // ---------------------------------------------------------
             // check for straigth, flush, straigth flush and royal flush
             participantVisibleHand.CurrentCards = participantVisibleHand.CurrentCards.OrderBy(c => c.Rank).ToList();
             int sequentialCards = 0;
             int sameSuites = 0;
-            ICard highestInSequence = null;
+            ICard highestInSequence = new Card(0, 'S');
 
             for (int i = 1; i < participantVisibleHand.CurrentCards.Count - 1; i++)
             {
-                if (participantVisibleHand.CurrentCards[i].Rank + 1 == participantVisibleHand.CurrentCards[i + 1].Rank
-                    || participantVisibleHand.CurrentCards[i].Rank - 1 == participantVisibleHand.CurrentCards[i - 1].Rank)
+                bool foundHigherSequential = false;
+
+                // cannot escape the checks for first and last element, otherwise they are skipped
+                if (i == 1)
                 {
-                    sequentialCards += 1;
-                    highestInSequence = participantVisibleHand.CurrentCards[i];
-                    if (participantVisibleHand.CurrentCards[i].Suit == participantVisibleHand.CurrentCards[i + 1].Suit)
+                    if (participantVisibleHand.CurrentCards[i - 1].Rank + 1 == participantVisibleHand.CurrentCards[i].Rank)
+                    {
+                        sequentialCards += 1;
+                    }
+                    if (participantVisibleHand.CurrentCards[i - 1].Suit == participantVisibleHand.CurrentCards[i].Suit)
                     {
                         sameSuites += 1;
                     }
                 }
-                else if (participantVisibleHand.CurrentCards[i].Suit == participantVisibleHand.CurrentCards[i + 1].Suit
-                        || participantVisibleHand.CurrentCards[i].Suit == participantVisibleHand.CurrentCards[i - 1].Suit)
+                else if (i == participantVisibleHand.CurrentCards.Count - 2)
+                {
+                    if (participantVisibleHand.CurrentCards[i].Rank + 1 == participantVisibleHand.CurrentCards[i + 1].Rank)
+                    {
+                        sequentialCards += 1;
+                        foundHigherSequential = true;
+                    }
+                    if (participantVisibleHand.CurrentCards[i].Suit == participantVisibleHand.CurrentCards[i].Suit)
+                    {
+                        sameSuites += 1;
+                    }
+                }
+                if (participantVisibleHand.CurrentCards[i].Rank + 1
+                        == participantVisibleHand.CurrentCards[i + 1].Rank
+                        || participantVisibleHand.CurrentCards[i].Rank - 1
+                        == participantVisibleHand.CurrentCards[i - 1].Rank)
+                {
+                    sequentialCards += 1;
+                    highestInSequence = participantVisibleHand.CurrentCards[i];
+                    if (participantVisibleHand.CurrentCards[i].Rank + 1 == participantVisibleHand.CurrentCards[i + 1].Rank)
+                    {
+                        foundHigherSequential = true;
+                    }
+                    if (participantVisibleHand.CurrentCards[i].Suit
+                        == participantVisibleHand.CurrentCards[i + 1].Suit)
+                    {
+                        sameSuites += 1;
+                    }
+                }
+                else if (participantVisibleHand.CurrentCards[i].Suit
+                         == participantVisibleHand.CurrentCards[i + 1].Suit
+                         || participantVisibleHand.CurrentCards[i].Suit
+                         == participantVisibleHand.CurrentCards[i - 1].Suit)
                 {
                     sameSuites += 1;
                     if (highestInSequence.Rank < participantVisibleHand.CurrentCards[i].Rank)
@@ -105,7 +85,7 @@
                     }
                 }
 
-                if (sequentialCards < 4 && sameSuites < 4)
+                if (foundHigherSequential)
                 {
                     highestInSequence = participantVisibleHand.CurrentCards[i + 1];
                 }
@@ -137,11 +117,76 @@
             }
             else if ((int)participantVisibleHand.Strength < (int)HandStrengthEnum.Pair)
             {
-                // last check is if no winning set of cards has been found -> set the highest card to the highest in the whole hand (in player hand and board)
+                // last check is if no winning set of cards has been found -> set the highest card to the highest in the player hand
                 participantVisibleHand.HighCard = startingTwoCards[0].Rank > startingTwoCards[1].Rank
                                                       ? startingTwoCards[0]
                                                       : startingTwoCards[1];
                 participantVisibleHand.Strength = HandStrengthEnum.HighCard;
+            }
+
+            // ----------------------------------------------------------
+            // begin search for pairs, two pairs, three of a kind, full house, four of a kind
+            IList<ICard> differentCards = new List<ICard>();
+
+            foreach (var card in participantVisibleHand.CurrentCards)
+            {
+                if (differentCards.Any(c => c.Rank == card.Rank))
+                {
+                    continue;
+                }
+
+                differentCards.Add(card);
+            }
+
+            // Check for pair, two pair, three of a kind, full house or four of a kind
+            for (int i = 0; i < differentCards.Count; i++)
+            {
+                int pairedCardsCounter = participantVisibleHand.CurrentCards.Count(card => card.Rank == differentCards[i].Rank);
+
+                if (pairedCardsCounter > 1)
+                {
+                    // four of a kind check
+                    if (pairedCardsCounter == 4)
+                    {
+                        participantVisibleHand.Strength = HandStrengthEnum.FourOfAKind;
+                        participantVisibleHand.HighCard = differentCards[i];
+                        break;
+                    }
+
+
+                    // check full house -> if not check two pair -> if not results in simple pair
+                    if (pairedCardsCounter == 3 
+                        && (participantVisibleHand.Strength == HandStrengthEnum.Pair
+                        || participantVisibleHand.Strength == HandStrengthEnum.TwoPair)
+                        && (int)participantVisibleHand.Strength < (int)HandStrengthEnum.FourOfAKind)
+                    {
+                        participantVisibleHand.Strength = HandStrengthEnum.FullHouse;
+                    }
+                    else if (pairedCardsCounter == 3 && (int)participantVisibleHand.Strength < pairedCardsCounter)
+                    {
+                        participantVisibleHand.Strength = HandStrengthEnum.ThreeOfAKind;
+                        participantVisibleHand.HighCard = differentCards[i];
+                    }
+                    else if (pairedCardsCounter == 2
+                            && participantVisibleHand.Strength == HandStrengthEnum.ThreeOfAKind
+                            && (int)participantVisibleHand.Strength < (int)HandStrengthEnum.FourOfAKind)
+                    {
+                        participantVisibleHand.Strength = HandStrengthEnum.FullHouse;
+                    }
+                    else if (participantVisibleHand.Strength == HandStrengthEnum.Pair)
+                    {
+                        participantVisibleHand.Strength = HandStrengthEnum.TwoPair;
+                        if (participantVisibleHand.HighCard.Rank < differentCards[i].Rank)
+                        {
+                            participantVisibleHand.HighCard = differentCards[i];
+                        }
+                    }
+                    else if(participantVisibleHand.Strength < HandStrengthEnum.Pair)
+                    {
+                        participantVisibleHand.Strength = HandStrengthEnum.Pair;
+                        participantVisibleHand.HighCard = differentCards[i];
+                    }
+                }
             }
         }
     }
